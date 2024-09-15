8382b917983d1527aeb3a811e2fba54538bb67b9
gladson-sza <gladsonsouza@live.com>
2024-09-15 10:18:48 -0400

    Feat: Add custom ActionBar

---
+++ app/src/main/java/com/gladson/weatherapp/ui/screen/WeatherScreen.kt
@@ -0,0 +1,32 @@
+package com.gladson.weatherapp.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.gladson.weatherapp.ui.components.ActionBar
+import com.gladson.weatherapp.ui.theme.ColorBackground
+
+@Composable
+fun WeatherScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = ColorBackground
+    ) { paddings ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddings)
+                .padding(
+                    horizontal = 24.dp,
+                    vertical = 10.dp
+                )
+        ) {
+            ActionBar()
+        }
+
+    }
+}
